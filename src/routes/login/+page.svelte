0c7974a532b7d2d127f434bbfc67fc3d65495949
Scharpp <lukehufnagel0@gmail.com>
2024-07-15 17:11:51 -0400

    Change file strucutre of login and update formatting

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,42 +1,8 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import Login from "./login.svelte";
+  import Login from "./LoginContainer.svelte";
 
-  let username:string;
-  let password:string;
-
-  const handleLogin = async () => {
-    try {
-      const response = await fetch('/auth/login', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          username,
-          password_hash: hash(password)
-        })
-      });
-      
-      const data = await response.json();
-      if (response.ok) {
-        // change later if home page changes 
-        console.log(response.json);
-        () => window.location.href = '/';
-      } else {
-        console.error('Login failed:', data.message);
-      }
-    } catch (error) {
-      console.error('Error during Login:', error)
-    }
-  }
-
-
-  // No Hashing yet, implement
-  function hash(password:string):string {
-    return password;
-  }
+  let username: string;
+  let password: string;
 </script>
 
-<Login on:click={handleLogin} {username} {password}/>
+<Login />
---
+++ src/routes/login/LoginContainer.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import LoginForm from "./LoginForm.svelte";
+</script>
+
+<div class="container">
+  <div class="login">
+    <h3>Login</h3>
+    <LoginForm />
+  </div>
+</div>
+
+<style>
+  .container {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+
+  .login {
+    max-width: 300px;
+    text-align: center;
+    border: 1px solid #ccc;
+    padding: 20px;
+    border-radius: 5px;
+  }
+
+  /* Maybe add this (or something similar) to a global stylesheet later */
+  button {
+    padding: 0.4em;
+    margin: 0 0 0.5em 0;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #999;
+  }
+</style>

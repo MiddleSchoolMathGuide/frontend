2a8c7cc6015b04db259b7403c24e912270bfea58
Scharpp <lukehufnagel0@gmail.com>
2024-07-15 17:18:53 -0400

    Fix form/container style

--- src/routes/login/LoginContainer.svelte
+++ src/routes/login/LoginContainer.svelte
@@ -26,18 +26,4 @@
     padding: 20px;
     border-radius: 5px;
   }
-
-  /* Maybe add this (or something similar) to a global stylesheet later */
-  button {
-    padding: 0.4em;
-    margin: 0 0 0.5em 0;
-    box-sizing: border-box;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-
-  button:hover {
-    background-color: #999;
-  }
 </style>

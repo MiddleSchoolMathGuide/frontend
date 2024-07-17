bcc0e413678f5cc5bb1ede193dc017f3b22ade2f
Scharpp <lukehufnagel0@gmail.com>
2024-07-17 00:27:46 -0400

    Change e-mail to username

--- src/routes/me/+page.svelte
+++ src/routes/me/+page.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { writable } from 'svelte/store';
+  import { onMount } from "svelte";
+  import { writable } from "svelte/store";
 
   let loggedIn = writable();
-  export let user = 'User';
+  export let user = "User";
 
   onMount(async () => {
-      const response = await fetch('/auth/is_logged_in');
-      if (response.ok) {
-        const data = await response.json();
-        loggedIn = data.set(data);
-      } else {
-        console.error('Failed login check: ' + response.statusText);
-      }
+    const response = await fetch("/auth/is_logged_in");
+    if (response.ok) {
+      const data = await response.json();
+      loggedIn = data.set(data);
+    } else {
+      console.error("Failed login check: " + response.statusText);
+    }
   });
 </script>
 

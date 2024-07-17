d33c4312e11ca094a988f5ed46c8d71df96f4715
Wrench56 <dmarkreg@gmail.com>
2024-07-17 02:52:17 -0400

    Add `/user/me` page

---
+++ src/lib/components/user/UserData.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import type { UserAPIResponse } from "$lib/types/profile.type";
+  import type { ResponseDataWrapper } from "$lib/types/wrapper.type";
+  import { timeConverter } from "$lib/utils/converter";
+
+  export let data: ResponseDataWrapper<UserAPIResponse>;
+</script>
+
+<h1>
+  Hello <span class={data.data.privilige_level}>{data.data.username}</span>!
+</h1>
+<p>Great to have you here!</p>
+
+<p>User created: {timeConverter(data.data.created_at)}</p>
+<p>User updated: {timeConverter(data.data.updated_at)}</p>
+
+<p>Email: {data.data.email}</p>
+
+<h2>You Profile</h2>
+<p>Bio: {data.data.profile.bio}</p>
+<p>Fullname: {data.data.profile.name}</p>
+
+<style>
+  .admin {
+    color: green;
+  }
+
+  .moderator {
+    color: red;
+  }
+
+  .user {
+    color: blue;
+  }
+</style>
--- src/routes/user/me/+page.svelte
+++ src/routes/user/me/+page.svelte
@@ -1,19 +1,37 @@
 <script lang="ts">
+  import UserData from "$lib/components/user/UserData.svelte";
+  import type { UserAPIResponse } from "$lib/types/profile.type";
+  import type { ResponseDataWrapper } from "$lib/types/wrapper.type";
   import { onMount } from "svelte";
-  import { writable } from "svelte/store";
 
-  let loggedIn = writable();
-  export let user = "User";
+  /* Default data */
+  let data: ResponseDataWrapper<UserAPIResponse> = {
+    ok: false,
+    msg: "Loading...",
+    data: {
+      username: "Mr. Default",
+      email: "default@email.com",
+      privilige_level: "user",
+      created_at: new Date(),
+      updated_at: new Date(),
+      profile: {
+        bio: "Lorem ipsum dolor sit amet",
+        name: "Default Default the III.",
+      },
+    },
+  };
 
   onMount(async () => {
-    const response = await fetch("/auth/is_logged_in");
-    if (response.ok) {
-      const data = await response.json();
-      loggedIn = data.set(data);
-    } else {
-      console.error("Failed login check: " + response.statusText);
-    }
+    fetch("/user/me/profile")
+      .then((response) => response.json())
+      .then((responseJson: ResponseDataWrapper<UserAPIResponse>) => {
+        if (responseJson.ok) {
+          data = responseJson;
+        } else {
+          console.error("Failed fetching profile: " + responseJson.msg);
+        }
+      });
   });
 </script>
 
-<h1>Hello {user}!</h1>
+<UserData {data} />

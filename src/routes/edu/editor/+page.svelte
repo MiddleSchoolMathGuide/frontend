15efd49ad90c4a0d8d5519758e522df2e47d0541
Wrench56 <dmarkreg@gmail.com>
2024-07-18 01:37:23 -0400

    Add `/edu/editor` page

---
+++ src/routes/edu/editor/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Topic from "$lib/components/edu/editor/Topic.svelte";
+  import { Status, type ITopic } from "$lib/types/edu.type";
+  import { onMount } from "svelte";
+
+  let topic: ITopic = {
+    description: "",
+    icon: "",
+    status: Status.Default,
+    title: "",
+    units: [],
+  };
+
+  onMount(() => {
+    const dynamicDataDiv = document.getElementById("dynamic-data");
+    if (dynamicDataDiv) {
+      const dataMessage = dynamicDataDiv.getAttribute("data-message");
+      if (dataMessage) {
+        /* TODO: Handle parsing error */
+        topic = JSON.parse(dataMessage);
+      }
+    }
+  });
+</script>
+
+<Topic {topic} />

1b3a89da8df7cc2e8fc6b8920b4eef87749d4931
Wrench56 <dmarkreg@gmail.com>
2024-07-18 01:37:12 -0400

    Add `Topic`, `Unit` and `Lesson` components

---
+++ src/lib/components/edu/editor/Unit.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { IUnit } from "$lib/types/edu.type";
+  import Lesson from "./Lesson.svelte";
+
+  export let unit: IUnit;
+</script>
+
+{#each unit.lessons ?? [] as lesson}
+  <Lesson {lesson} />
+{/each}

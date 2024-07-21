e3a9786bcf9da28679663bcf1f9e4cf07c44c352
Scharpp <lukehufnagel0@gmail.com>
2024-07-21 17:34:44 -0400

    Fix colors for Warren

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,24 @@
 <script>
-  import Header from './Header.svelte';
+  import Header from "./Header.svelte";
 </script>
 
 <Header />
+<div class="home-background"></div>
+
+<style>
+  body,
+  html {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
+
+  .home-background {
+    background-color: #f2f2f2;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>

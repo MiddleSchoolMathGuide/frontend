a810d23ec0a14f5b122fa1b101f139babc5a2ea1
Scharpp <lukehufnagel0@gmail.com>
2024-07-21 22:43:10 -0400

    Add homepage

---
+++ src/routes/Home.svelte
@@ -0,0 +1,63 @@
+<div class="container">
+  <div class="title">
+    <h1>MSMG</h1>
+  </div>
+
+  <div class="description">
+    <p>
+      <strong
+        >The world's first free centralized resource for middle school math
+        competition preparation!</strong
+      >
+    </p>
+  </div>
+
+  <div class="start">
+    <a href="/login" class="get-started-link">Get Started</a>
+  </div>
+</div>
+
+<style>
+  .container {
+    margin-top: -500px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+  }
+
+  .start {
+    width: fit-content;
+    height: fit-content;
+    border: 1px solid;
+    border-radius: 5px;
+    margin-top: 40px;
+    margin-bottom: 20px;
+    padding: 7px;
+    text-align: center;
+  }
+
+  .description {
+    border-bottom: 2px solid red;
+    padding-bottom: 10px;
+    border-radius: 0 0 5px 5px;
+    text-align: center;
+  }
+
+  .title {
+    font-size: 40px;
+    line-height: 1;
+    padding: 0;
+    margin-bottom: 15px;
+  }
+
+  .get-started-link {
+    color: black;
+    text-decoration: none;
+    padding: 10px 20px;
+  }
+</style>

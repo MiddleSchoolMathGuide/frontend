931ec8fc835578572e08524598e4f854fc827164
Wrench56 <dmarkreg@gmail.com>
2024-07-28 02:24:25 -0400

    Fix `signup` link

--- src/routes/login/LoginForm.svelte
+++ src/routes/login/LoginForm.svelte
@@ -56,7 +56,7 @@
 </div>
 <div class="form-control">
   <button on:click={() => handleLogin()} class="login-button">Login</button>
-  <p style="font-size: 10px"><a href="../signIn">No account? Make one here</a></p>
+  <p style="font-size: 10px"><a href="/signup">No account? Make one here</a></p>
 </div>
 
 <style>

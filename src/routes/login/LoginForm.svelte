1eaeaad963cabb5f5e8d94e34b7e21bd04a6526d
Wrench56 <dmarkreg@gmail.com>
2024-07-28 04:11:47 -0400

    Create `crypto.ts` util

--- src/routes/login/LoginForm.svelte
+++ src/routes/login/LoginForm.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { sha256 } from "$lib/utils/crypto";
+
   let username: string = "";
   let password: string = "";
 
@@ -28,14 +30,6 @@
       console.error("Error during Login:", error);
     }
   };
-
-  async function sha256(data: string): Promise<string> {
-    return Array.from(
-      new Uint8Array(
-        await crypto.subtle.digest("SHA-256", new TextEncoder().encode(data))
-      )
-    ).reduce((acc, b) => acc + b.toString(16).padStart(2, "0"), "");
-  }
 </script>
 
 <div class="form-control">

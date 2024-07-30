4c786e2b3e28ac4cc5081a9602579cf5e2d1ebe2
Wrench56 <dmarkreg@gmail.com>
2024-07-30 04:04:46 -0400

    Add `EHeader.svelte` and `ESubHeader.svelte`

---
+++ src/lib/components/topic/editor/widgets/ESubHeader.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { WSubHeader } from "$lib/types/widgets.type";
+
+  export let widget: WSubHeader;
+</script>
+
+<div class="widget-edit-form">
+  <label for="text">Text</label>
+  <input
+    id="text"
+    type="text"
+    bind:value={widget.text}
+    placeholder="Enter widget text"
+  />
+</div>

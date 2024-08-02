274d6daf08d9fb3d89d9830789ed1b626f68fef1
Scharpp <lukehufnagel0@gmail.com>
2024-08-01 21:21:16 -0400

    Add Difficulty component

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -73,7 +73,7 @@
         {#each options as _option}
           <li>
             <!-- SvelteKit tries to resolve the links that leads to errors. -->
-            
+
             <a
               href="/{_option}"
               on:click|preventDefault={() => navigateTo(_option)}
@@ -86,7 +86,6 @@
               {#if _option.length === 0}HOME
               {:else}{_option.toUpperCase()}{/if}
             </a>
-           
           </li>
         {/each}
       </ul>

b48de1542520d1689c8aa31d228154747fa93d12
Wrench56 <dmarkreg@gmail.com>
2024-08-25 07:00:36 -0400

    Add `lesson` and `context` loading in `GeneralLesson.svelte`

--- src/routes/lesson/GeneralLesson.svelte
+++ src/routes/lesson/GeneralLesson.svelte
@@ -3,6 +3,11 @@
   import LessonBar from "$lib/components/lesson/LessonBar.svelte";
   import ProgressBar from "$lib/components/lesson/ProgressBar.svelte";
   import TableOfContents from "$lib/components/lesson/TableOfContents.svelte";
+  import Header from "$lib/components/lesson/widgets/Header.svelte";
+  import SubHeader from "$lib/components/lesson/widgets/SubHeader.svelte";
+  import type { ILesson, ITopic, IUnit } from "$lib/types/topic.type";
+  import { WidgetType } from "$lib/types/widgets.type";
+  import type { ResponseDataWrapper } from "$lib/types/wrapper.type";
   import { onMount } from "svelte";
 
   export let names: {
@@ -17,34 +22,98 @@
     programmingNames: [""],
   };
 
-  export let contents: string[] = [""];
+  let units: Array<IUnit> = [];
+  let lessons: Array<ILesson> = [];
+  let topic: ITopic;
+  let lesson: ILesson;
 
-  let docHeight: number = 0;
+  let loaded: boolean = false;
 
   onMount(() => {
-    docHeight = document.documentElement.scrollHeight;
+    let pathparts = window.location.pathname.slice(3).split("/");
+    const topic_title = pathparts[0];
+    const unit_title = pathparts[1];
+    const lesson_title = pathparts[2];
+
+    /* Fetch units */
+    const units_promise = fetch(`/api/list/${topic_title}`)
+      .then((data) => data.json())
+      .then((respone: ResponseDataWrapper<Array<IUnit>>) => {
+        if (!respone.ok) {
+          /* TODO: Error handling */
+          console.error("Error while loading units list");
+          return;
+        }
+
+        units = respone.data;
+      });
+
+    /* Fetch lessons */
+    const lessons_promise = fetch(`/api/list/${topic_title}/${unit_title}`)
+      .then((data) => data.json())
+      .then((respone: ResponseDataWrapper<Array<ILesson>>) => {
+        if (!respone.ok) {
+          /* TODO: Error handling */
+          console.error("Error while loading lessons list");
+          return;
+        }
+
+        lessons = respone.data;
+      });
+
+    /* Fetch whole lesson */
+    const lesson_promise = fetch(
+      `/api/t/${topic_title}/${unit_title}/${lesson_title}`
+    )
+      .then((data) => data.json())
+      .then((respone: ResponseDataWrapper<ITopic>) => {
+        console.log(respone);
+        if (!respone.ok) {
+          /* TODO: Error handling */
+          console.error("Error while loading lesson");
+          return;
+        }
+
+        topic = respone.data;
+        lesson = topic.units[0].lessons[0];
+      });
+
+    Promise.all([units_promise, lessons_promise, lesson_promise]).then(() => {
+      loaded = true;
+    });
   });
 </script>
 
-<div class="lesson-container">
-  <div class="left-materials">
-    <LessonBar />
+{#if loaded}
+  <div class="lesson-container">
+    <div class="left-materials">
+      <LessonBar />
+    </div>
+    <!-- Central Div for Widgets -->
+    <div class="central-widgets">
+      {#each lesson.widgets as widget (widget._id)}
+        {#if widget.type === WidgetType.Header}
+          <Header {widget} />
+        {:else if widget.type === WidgetType.SubHeader}
+          <SubHeader {widget} />
+        {/if}
+      {/each}
+    </div>
+    <div class="right-materials">
+      <div class="credits">
+        <Credits {names} />
+      </div>
+      <div class="table-of-contents">
+        <TableOfContents widgets={lesson.widgets} />
+      </div>
+      <div class="progress-bar">
+        <ProgressBar />
+      </div>
+    </div>
   </div>
-  <!-- Central Div for Widgets -->
-  <div class="central-widgets"></div>
-
-  <div class="right-materials">
-    <div class="credits">
-      <Credits {names} />
-    </div>
-    <div class="table-of-contents">
-      <TableOfContents {contents} />
-    </div>
-    <div class="progress-bar">
-      <ProgressBar docHeight={docHeight} />
-    </div>
-  </div>
-</div>
+{:else}
+  <!-- Spinner -->
+{/if}
 
 <style>
   .lesson-container {

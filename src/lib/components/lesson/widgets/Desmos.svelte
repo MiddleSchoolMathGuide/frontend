c3d6c45442786d1e20aeee1cbf33e865e056fbf6
Scharpp <lukehufnagel0@gmail.com>
2024-09-06 21:50:13 -0400

    attempt fix on Desmos.svelte

--- src/lib/components/lesson/widgets/Desmos.svelte
+++ src/lib/components/lesson/widgets/Desmos.svelte
@@ -9,42 +9,32 @@
   let height: number =
     widget.height != undefined && widget.height > 0 ? widget.height : 500;
 
-  //This is one of the few times where Svelte makes anything harder...
-  function loadDesmosScript(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      if (typeof window !== "undefined" && (window as any).Desmos) {
-        resolve((window as any).Desmos);
-      } else {
-        const script = document.createElement("script");
-        script.src =
-          "https://www.desmos.com/api/v1.9/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6";
-        script.onload = () => {
-          resolve((window as any).Desmos);
-        };
-        script.onerror = () =>
-          reject(new Error("Failed to load Desmos script"));
-        document.head.appendChild(script);
-      }
-    });
-  }
+  onMount(() => {
+    let desmosScript = document.createElement("script");
+    desmosScript.src =
+      "https://www.desmos.com/api/v1.9/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6";
 
-  onMount(async () => {
-    try {
-      const Desmos = await loadDesmosScript();
-      let elt = document.getElementById("desmos");
-      if (elt && Desmos) {
-        let calculator = Desmos.GraphingCalculator(elt);
-        calculator.setExpression({
-          id: "given",
-          latex: widget.inputFunction !== null ? widget.inputFunction : "",
-        });
+    desmosScript.onload = () => {
+      if (window.Desmos) {
+        const elt = document.getElementById("desmos");
+        if (elt) {
+          const calculator = window.Desmos.GraphingCalculator(elt, {
+            expressions: true,
+          });
+
+          if (widget.inputFunction) {
+            calculator.setExpression({
+              id: "givenLine",
+              latex: widget.inputFunction,
+            });
+          }
+        }
       } else {
-        console.error("Desmos API is not available");
+        console.error("Desmos library is not loaded.");
       }
-    } catch (error) {
-      console.error("Error loading Desmos script:", error);
-    }
+    };
+    document.head.appendChild(desmosScript);
   });
 </script>
 
-<div id="desmos" style="width:{width}px heigth:{height}px"></div>
+<div id="desmos" style="width:{width}px; height:{height}px;"></div>

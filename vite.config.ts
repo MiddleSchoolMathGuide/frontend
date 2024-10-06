bc56db553bc2d7b114d3d32cb803e67b2fdd75cb
Wrench56 <markdevenyidev@gmail.com>
2024-10-06 01:53:33 -0400

    Optimize bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		fs: {
-			allow: ['..'],
-		}
-	}
+  plugins: [sveltekit()],
+  server: {
+    fs: {
+      allow: [".."],
+    },
+  },
+  define: {
+    /* Remove this if you want the `code` plugin to work */
+    __ENABLE_CARTA_SSR_HIGHLIGHTER__: false,
+  },
 });

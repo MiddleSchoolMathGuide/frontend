ff24777172a67d63cceba031dc14ac145910ee2d
Wrench56 <markdevenyidev@gmail.com>
2024-10-05 23:18:17 -0400

    List topics on page `/topic`

--- src/routes/topic/+page.svelte
+++ src/routes/topic/+page.svelte
@@ -1,14 +1,31 @@
-<script>
-  import { onMount } from 'svelte';
+<script lang="ts">
+  import type { ITopicMinimal, IUnitMinimal } from "$lib/types/topic.type";
+  import type { ResponseDataWrapper } from "$lib/types/wrapper.type";
+  import { onMount } from "svelte";
+
+  let topics: Array<ITopicMinimal>;
 
   function navigateToNewTopic() {
     window.location.href = "/topic/editor?topic=new";
   }
 
+  function get_topics() {
+    fetch("/api/list")
+      .then((data) => data.json())
+      .then((topics_: ResponseDataWrapper<Array<ITopicMinimal>>) => {
+        if (topics_.ok === false) {
+          console.error(`Error during /api/list call: ${topics_.msg}`);
+          return;
+        }
+        topics = topics_.data;
+      });
+  }
+
   onMount(() => {
-        const button = document.querySelector('.centered-button');
-        if (button !== null) button.classList.add('gradient-transition');
-    });
+    const button = document.querySelector(".centered-button");
+    if (button !== null) button.classList.add("gradient-transition");
+    get_topics();
+  });
 </script>
 
 <div class="container">
@@ -16,6 +33,13 @@
     New Topic
   </button>
 </div>
+
+{#if topics}
+  {#each topics as topic}
+    <h1>{topic.title}</h1>
+    <p>{topic.description}</p>
+  {/each}
+{/if}
 
 <style>
   .container {
@@ -33,7 +57,7 @@
     text-align: center;
     border: none;
     border-radius: 5px;
-    background: linear-gradient(45deg, #007BFF, #00CFFF);
+    background: linear-gradient(45deg, #007bff, #00cfff);
     color: white;
     cursor: pointer;
     transition: background-color 0.3s;
